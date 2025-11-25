db195b1d112686cf0b1e149f91aa2376d6b512b1
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-25 14:49:22 +0530

    Add example script for string manipulation
    Introduces changes.js demonstrating variable assignment, string concatenation, template literals, and property access in JavaScript.

---
+++ 006 JavaScript/part1/changes.js
@@ -0,0 +1,8 @@
+let gameName="Chess";
+console.log("I love playing " + gameName + "!");
+
+console.log(`I love playing ${gameName}!`);
+console.log(`The length of the game name is ${gameName.length}.`);
+
+gameName = "Soccer";
+console.log(`Now I love playing ${gameName}!`);
