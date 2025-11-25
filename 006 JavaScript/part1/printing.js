bdff1e5f6223f1bd78ab0758649ca52340baa3b7
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-25 14:39:27 +0530

    Add basic HTML and JS for console output demo
    Introduces index.html and printing.js to demonstrate various JavaScript console methods, including log, table, error, and warn. Sets up a simple HTML page that loads the script.

---
+++ 006 JavaScript/part1/printing.js
@@ -0,0 +1,5 @@
+console.log("Hello, world!");
+// process.stdout.write("Hello, world!");
+console.table({greeting: "Hello, world!"});
+console.error("Hello, world!");
+console.warn("Hello, world!");
