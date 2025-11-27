e1fa22ee9214e6607895c70d75730a139643db3d
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-11-27 18:43:13 +0530

    Add conditional challenge examples in JavaScript
    Introduces a new file with examples demonstrating conditional logic in JavaScript, including number comparison, string equality, boolean checks, and array emptiness checks. These examples are commented out for instructional purposes.

---
+++ 006 JavaScript/part3/conditional-challenges.js
@@ -0,0 +1,54 @@
+//check if a num is greater than or not
+
+/*
+let num1=34
+let num2=23
+
+if (num1 > num2) {
+    console.log(`${num1} is greater than ${num2}`);
+}else{
+    console.log(`${num2} is greater than ${num1}`);
+}
+
+console.log("i am a regular bottom code");
+
+
+
+let username="gaurav"
+let anotherUsername="Gaurav"
+
+if(username == anotherUsername){
+    console.log("yes , it is same");
+}else{
+    console.log("no , it is not same");
+}
+
+console.log(typeof username);
+console.log(typeof anotherUsername);
+
+*/
+
+
+//check if a boolean value is true or not
+
+/*
+
+let isReady=true
+
+if(isReady){
+    console.log("yes , it is true")
+}
+    
+
+
+//check if an array is empty or not
+
+let item=[]
+console.log(item.length);
+
+if(item.length==0){
+    console.log("array is empty");
+    
+}
+
+*/
