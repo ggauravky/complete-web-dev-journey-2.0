8c5050616f1dad4fe96c6d343b99c91d10f22828
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-12-04 11:21:02 +0530

    Add OOP concepts: encapsulation, abstraction, polymorphism
    Introduced examples for encapsulation (BankAccount), abstraction (cofeeMachine), polymorphism (Bird/Eagle), static methods (calculator), and getters/setters (Employee). Commented out previous method calls for Truck and Animal instances.

--- 006 JavaScript/part6/oops=master.js
+++ 006 JavaScript/part6/oops=master.js
@@ -32,8 +32,8 @@ Array.prototype=new Truck();
 
 let truck1=new Truck("Ford","2018");
 let truck2=new Truck("Chevrolet","2022");
-truck1.start();
-truck2.start();
+//truck1.start();
+//truck2.start();
 
 class Animal{
     constructor(name, species){
@@ -48,5 +48,102 @@ class Animal{
 let animal1=new Animal("Leo","Lion");
 let animal2=new Animal("Molly","Cat");
 
-animal1.info();
-animal2.info();
+//animal1.info();
+//animal2.info();
+
+
+//encapsulation
+
+class BankAccount{
+    #balance=0; //private field
+
+    deposit(amount){
+        this.#balance+=amount;
+        return this.#balance;
+    }
+    getBalance(){
+        return `Current balance is: ${this.#balance}`;
+    }
+}
+
+let myAccount=new BankAccount();
+myAccount.deposit(500);
+//console.log(myAccount.getBalance());
+//console.log(myAccount.#balance); //error: private field
+
+
+//anbstraction
+
+
+class cofeeMachine{
+    start(){
+        this.boilWater();
+        this.brewCofee();
+        console.log("Cofee is ready!");
+    }
+    brewCofee(){
+        console.log("Brewing cofee...");
+    }
+    boilWater(){ //private method
+        console.log("Boiling water...");
+    }
+}
+
+let myCofeeMachine=new cofeeMachine();
+//myCofeeMachine.start();
+
+
+//polymorphism
+
+class Bird{
+    fly(){
+        return "Flying high!";
+    }
+}
+
+class Eagle extends Bird{
+    fly(){
+        return "Eagle soaring high!";
+    }
+}
+
+let myBird=new Bird();
+let myEagle=new Eagle();
+//console.log(myBird.fly());
+//console.log(myEagle.fly());
+
+
+//static methods 
+
+class calculator{
+    static add(a,b){
+        return a+b;
+    }
+}
+
+let sum=calculator.add(5,10);
+//console.log("Sum is: " + sum);
+
+
+//getters and setters
+
+class Employee{
+    constructor(name, salary){
+        this.name=name;
+        this._salary=salary; //private variable by convention
+    }
+    get salary(){
+        return this._salary;
+    }
+    set salary(newSalary){
+        if(newSalary > 0){
+            this._salary=newSalary;
+        } else {
+            console.log("Invalid salary amount");
+        }
+    }
+}
+let emp=new Employee("Gaurav",5000);
+//console.log(emp.salary); //getter
+emp.salary=6000; //setter
+//console.log(emp.salary);
