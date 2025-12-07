5de2914fc5c3088e4b9b7825e8cc372e94514675
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-12-07 11:53:14 +0530

    Add event listener examples for DOM interactions
    Added examples demonstrating event listeners for mouseover, click, form submission, DOMContentLoaded, and toggling classes. These examples illustrate common DOM manipulation and event handling patterns in JavaScript.

--- 006 JavaScript/part8/script.js
+++ 006 JavaScript/part8/script.js
@@ -42,3 +42,46 @@ document.getElementById("removeLastTask").addEventListener("click",()=>{
     let tasklist=document.getElementById("taskList")
     tasklist.lastElementChild.remove()
 })
+
+//example 6:
+
+document.getElementById("ClickMeButton").addEventListener("mouseover",()=>{
+    alert("You hovered over the button!")
+})
+
+//example 7:
+
+document.getElementById("teaList").addEventListener("click",(event)=>{
+    // console.log(event);
+    if(event.target && event.target.nodeName==="LI"){
+        event.target.classList.toggle("highlight")
+    }
+})
+
+
+//example 8
+
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let feedback = document.getElementById("feedbackInput").value;
+    console.log(feedback);
+    document.getElementById(
+      "feedbackDisplay"
+    ).textContent = `Feedback is: ${feedback}`;
+  });
+
+//example 9
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "DOM fully loaded";
+});
+
+//example 10
+document
+  .getElementById("toggleHighlight")
+  .addEventListener("click", function () {
+    let descriptionText = document.getElementById("descriptionText");
+    descriptionText.classList.toggle("highlight");
+  });
