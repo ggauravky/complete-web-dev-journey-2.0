0243c393d32306199213b5fde37e7634677158a4
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2025-12-31 10:04:33 +0530

    Add explore.js for advanced JavaScript concepts
    Introduces a new script demonstrating advanced JavaScript features, including function definition, setTimeout usage, and a for loop for iteration.

---
+++ 006 JavaScript/part9_Advance/explore.js
@@ -0,0 +1,14 @@
+console.log("Exploring advanced JavaScript concepts!");
+
+function sayHello() {
+    console.log("Hello,! Welcome to advanced JavaScript.");
+}
+
+setTimeout(() => {
+    sayHello()
+}, 5000);
+
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
