5f2aea675b83eb0c86008089a9d0ebf75bb8c269
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-07 18:50:22 +0530

    Add example of generator and iterator in JavaScript
    Introduces a simple generator function and demonstrates iteration using both .next() and for...of loop.

---
+++ 006 JavaScript/part9_Advance/generatorsIterator.js
@@ -0,0 +1,17 @@
+function* numberGenerator() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = numberGenerator();
+
+console.log(gen.next()); // { value: 1, done: false }
+console.log(gen.next()); // { value: 2, done: false }
+console.log(gen.next()); // { value: 3, done: false }
+console.log(gen.next()); // { value: undefined, done: true }
+
+// Using for...of to iterate over the generator
+for (const num of numberGenerator()) {
+    console.log(num); // 1, then 2, then 3
+}
