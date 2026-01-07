ff0310b03391de488219303097771c0fecf4d399
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-07 18:57:58 +0530

    Add math operations module and usage examples
    Introduced mathOperationaM.js with add, subtract, and multiply functions supporting both ES module and CommonJS exports. Added appC.js (CommonJS) and appM.js (ES module) to demonstrate usage of these math functions.

---
+++ 006 JavaScript/part9_Advance/appC.js
@@ -0,0 +1,6 @@
+require("./mathOperationaM.js");
+console.log("App C is running");
+
+console.log("Addition: ", add(5, 3));
+console.log("Subtraction: ", subtract(5, 3));
+console.log("Multiplication: ", multiply(5, 3));
---
+++ 006 JavaScript/part9_Advance/appM.js
@@ -0,0 +1,7 @@
+import multiply from "./mathOperationaM.js";
+
+console.log(multiply(4, 5));
+import { add, subtract } from "./mathOperationaM.js";
+
+console.log(add(10, 20));
+console.log(subtract(30, 15));
