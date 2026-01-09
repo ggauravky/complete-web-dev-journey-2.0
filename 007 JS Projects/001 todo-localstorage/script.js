be9a71eeae8c8b4883b3cf0bfc057487ff5c63b9
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-09 19:28:54 +0530

    Add initial To-Do List app with dark mode styling
    Introduces a basic to-do list application with HTML structure, dark-themed CSS styles, and initial JavaScript for adding tasks. The JavaScript currently sets up event listeners and task creation logic, though there is a bug where 'taskText.push(newTask)' should be 'tasks.push(newTask)'.

---
+++ 007 JS Projects/001 todo-localstorage/script.js
@@ -0,0 +1,19 @@
+const addTaskButton=document.getElementById("add-task-btn")
+const toDoList=document.getElementById("todo-list")
+const toDoInput=document.getElementById("todo-input")
+
+let tasks=[]
+addTaskButton.addEventListener("click",()=>{
+    const taskText=toDoInput.value.trim();
+    if(taskText==="") return;
+
+    const newTask={
+        id: Date.now(),
+        text:taskText,
+        complete:false,
+    };
+
+    taskText.push(newTask);
+    toDoInput.value=""
+    console.log(tasks)
+})
