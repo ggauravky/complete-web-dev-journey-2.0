91cfe49f2b14c4b21e14a49d9e719710ed50d389
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-12 16:37:15 +0530

    Add persistent todo list with localStorage support
    Refactored the todo app to load and save tasks from localStorage, ensuring tasks persist across page reloads. Added rendering of tasks on page load, task completion toggling, and task deletion with updates to localStorage.

--- 007 JS Projects/001 todo-localstorage/script.js
+++ 007 JS Projects/001 todo-localstorage/script.js
@@ -1,19 +1,56 @@
-const addTaskButton = document.getElementById("add-task-btn");
-const toDoList = document.getElementById("todo-list");
-const toDoInput = document.getElementById("todo-input");
+document.addEventListener("DOMContentLoaded", () => {
+  const addTaskButton = document.getElementById("add-task-btn");
+  const toDoList = document.getElementById("todo-list");
+  const toDoInput = document.getElementById("todo-input");
 
-let tasks = [];
-addTaskButton.addEventListener("click", () => {
-  const taskText = toDoInput.value.trim();
-  if (taskText === "") return;
+  let tasks =JSON.parse(localStorage.getItem('tasks')) || [];
 
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    complete: false,
-  };
+  tasks.forEach((task) => renderTasks(task));
 
-  tasks.push(newTask);
-  toDoInput.value = "";
-  console.log(tasks);
+  addTaskButton.addEventListener("click", () => {
+    const taskText = toDoInput.value.trim();
+    if (taskText === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      text: taskText,
+      complete: false,
+    };
+
+    tasks.push(newTask);
+    saveTasks();
+    renderTasks(newTask);
+    toDoInput.value = "";
+    console.log(tasks);
+  });
+
+  function renderTasks(task) {
+    const li=document.createElement('li');
+    li.setAttribute('data-id',task.id);
+    if(task.complete) li.classList.add("Completed")
+    li.innerHTML=`
+    <span>${task.text}</span>
+    <button>Delete</button>
+    `;
+    li.addEventListener('click',(e)=>{
+      if(e.target.tagName==='BUTTON') return ;
+      task.completed=!task.completed
+      li.classList.toggle('completed')
+      saveTasks()
+    });
+
+    li.querySelector('button').addEventListener('click',(e)=>{
+      e.stopPropagation()
+      tasks=tasks.filter(t=> t.id != task.id)
+      li.remove();
+      saveTasks();
+    })
+
+    toDoList.appendChild(li);
+    
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 });
